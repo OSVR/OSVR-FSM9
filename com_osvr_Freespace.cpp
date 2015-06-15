13d9e2b8f3d39079d57c780cbbb0cd8ddf4a59da
Ryan Pavlik <ryan@sensics.com>
2015-06-15 18:36:32 -0500

    Clean up code, including handling multiple devices safely, etc.

--- com_osvr_Freespace.cpp
+++ com_osvr_Freespace.cpp
@@ -50,260 +50,311 @@ Sensics, Inc.
 // Standard includes
 #include <iostream>
 #include <memory>
-
+#include <set>
+#include <array>
+#include <assert.h>
 
 // Anonymous namespace to avoid symbol collision
 namespace {
-	class FreespaceDevice {
-	public:
-		FreespaceDevice(OSVR_PluginRegContext ctx, FreespaceDeviceId freespaceId, 
-		struct FreespaceDeviceInfo* deviceInfo, const char* name) {
-			memcpy(&_deviceInfo, deviceInfo, sizeof(_deviceInfo));
-			_deviceId = freespaceId;
-			// 5 buttons + a scroll wheel
-			//vrpn_Button::num_buttons = 5;
-			//vrpn_Dial::num_dials = 1;
-			//memset(&_lastBodyFrameTime, 0, sizeof(_lastBodyFrameTime));
-			//_timestamp.tv_sec = 0;
 
-			/// Create the initialization options
-			OSVR_DeviceInitOptions opts = osvrDeviceCreateInitOptions(ctx);
+/// @brief Simple class holding the lifetime of the freespace lib
+class FreespaceLib {
+  public:
+    FreespaceLib() {
+        auto ret = freespace_init();
+        if (ret != FREESPACE_SUCCESS) {
+            throw std::runtime_error(
+                "Failed to initialize libfreespace, code was " +
+                std::to_string(ret));
+        }
+    }
+    ~FreespaceLib() { freespace_exit(); }
+};
 
-			/// Indicate that we'll report tracking.
-			osvrDeviceTrackerConfigure(opts, &m_tracker);
+/// @brief RAII wrapper for opening a libfreespace device
+class FreespaceDeviceIdWrapper {
+  public:
+    /// @brief Default constructor
+    FreespaceDeviceIdWrapper() = default;
 
-			/// Create an asynchronous (threaded) device
-			m_dev.initAsync(ctx, name, opts);
+    /// @brief Constructor from ID - tries to open the device.
+    explicit FreespaceDeviceIdWrapper(FreespaceDeviceId id) : m_id(id) {
+        auto ret = freespace_openDevice(m_id);
+        m_valid = (FREESPACE_SUCCESS == ret);
+        if (!m_valid) {
+            std::cout << "Error in freespace_openDevice, code " << ret
+                      << std::endl;
+        }
+    }
 
-			/// Send JSON descriptor
-			m_dev.sendJsonDescriptor(com_osvr_Freespace_json);
-		
-			printf("Registering update callback deviceId=%d\n", _deviceId);
-			/// Sets the update callback
-			m_dev.registerUpdateCallback(this);
-		}
+    /// @brief Move constructor
+    FreespaceDeviceIdWrapper(FreespaceDeviceIdWrapper &&other)
+        : m_id(other.m_id), m_valid(other.m_valid) {
+        other.m_valid = false;
+    }
 
-		/// In this case, the core spawns a thread, with a loop calling this
-		/// function as long as things are running. So this function waits for the
-		/// next message from the device and passes it on.
-		OSVR_ReturnCode update() {
+    /// @brief move assignable
+    FreespaceDeviceIdWrapper &operator=(FreespaceDeviceIdWrapper &&other) {
+        close();
+        m_id = other.m_id;
+        m_valid = other.m_valid;
+        other.m_valid = false;
+        return *this;
+    }
 
-			// A loop to read messages
-			//printf("Listening for messages on device with id %d\n", _deviceId);
-			struct freespace_message message;
-				rc = freespace_readMessage(_deviceId, &message, 100);
-				if (rc == FREESPACE_ERROR_TIMEOUT ||
-					rc == FREESPACE_ERROR_INTERRUPTED) {
-					// Both timeout and interrupted are ok.
-					// Timeout happens if there aren't any events for a second.
-					// Interrupted happens if you type CTRL-C or if you
-					// type CTRL-Z and background the app on Linux.
-					//continue;
-					//printf("FREESPACE_ERROR_TIMEOUT\n", rc);
-				}
-				if (rc != FREESPACE_SUCCESS && rc != FREESPACE_ERROR_TIMEOUT && rc != FREESPACE_ERROR_INTERRUPTED) {
-					// Close communications with the device
-					printf("Cleaning up...\n");
-					freespace_closeDevice(_deviceId);
+    /// @brief Destructor
+    ~FreespaceDeviceIdWrapper() { close(); }
 
-					// Cleanup the library
-					freespace_exit();
-					return OSVR_RETURN_FAILURE;
-					//break;
-				}
+    /// @brief non-copyable
+    FreespaceDeviceIdWrapper(FreespaceDeviceIdWrapper const &other) = delete;
 
-				// freespace_printMessage(stdout, &message); // This just prints the basic message fields
-				if (message.messageType == FREESPACE_MESSAGE_MOTIONENGINEOUTPUT) {
-					//rc = freespace_util_getAngularVelocity(&message.motionEngineOutput, &angVel);
-					rc = freespace_util_getAngPos(&message.motionEngineOutput, &angPos);
-					if (rc == 0) {
-						//printf("X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", angVel.x, angVel.y, angVel.z);
-						OSVR_OrientationState orientation;// = { angPos.w, angPos.x, -angPos.y, angPos.z };
-						osvrQuatSetIdentity(&orientation);
-						osvrQuatSetW(&orientation, angPos.w);
-						osvrQuatSetX(&orientation, angPos.x);
-						osvrQuatSetY(&orientation, angPos.y);
-						osvrQuatSetZ(&orientation, angPos.z);
-						osvrDeviceTrackerSendOrientation(m_dev, m_tracker, &orientation, 0);
-					}		
-				}
-				else
-				{
-					printf("unhandled message type:%d\n", message.messageType);
-				}
+    /// @brief not copy assignable
+    FreespaceDeviceIdWrapper &
+    operator=(FreespaceDeviceIdWrapper const &other) = delete;
 
-			return OSVR_RETURN_SUCCESS;
-		}
+    /// @brief Check validity/"is open"
+    explicit operator bool() const { return m_valid; }
 
-	private:
-		osvr::pluginkit::DeviceToken m_dev;
-		OSVR_TrackerDeviceInterface m_tracker;
-		//osvr::util::time::TimeValue m_timestamp;
-		FreespaceDeviceInfo _deviceInfo;
-		FreespaceDeviceId _deviceId;
-		int rc;
-		
-		struct MultiAxisSensor angPos;
-	};
-		
-	class HardwareDetection {
-	public:
-		HardwareDetection() : _freespace_initialized(false), m_found(false){}
-		OSVR_ReturnCode operator()(OSVR_PluginRegContext ctx) {
-			std::cout << "PLUGIN: Got a hardware detection request" << std::endl;
-			if (!m_found){
-				//initialize the freespace library
-				freespaceInit();
-				if (rc != FREESPACE_SUCCESS) {
-					printf("Initialization error. rc=%d\n", rc);
-					return OSVR_RETURN_FAILURE;
-				}
-				
-				int numIds = 0;	// The number of device Ids found
-				FreespaceDeviceId devices[FREESPACE_MAXIMUM_DEVICE_COUNT];
+    /// @brief Get the device ID
+    FreespaceDeviceId get() const {
+        assert(m_valid &&
+               "get() only makes sense if the ID is actually validly opened");
+        return m_id;
+    }
 
-				printf("Scanning for Freespace devices...\n");
-				// Get the list of availble devices
-				rc = freespace_getDeviceList(devices, FREESPACE_MAXIMUM_DEVICE_COUNT, &numIds);
-				if ((rc != FREESPACE_SUCCESS)) {
-					printf("Didn't find any devices.\n");
-					return OSVR_RETURN_FAILURE;
-				}
+    /// @brief Close device manually
+    void close() {
+        if (m_valid) {
+            freespace_closeDevice(m_id);
+            m_valid = false;
+        }
+    }
 
-				printf("Found %d devices ", numIds);
+  private:
+    FreespaceDeviceId m_id;
+    bool m_valid = false;
+};
 
-				FreespaceDeviceId freespaceId = devices[0];
-				struct FreespaceDeviceInfo deviceInfo;
-				// Retrieve the information for the device
-				rc = freespace_getDeviceInfo(freespaceId, &deviceInfo);
-				if (rc != FREESPACE_SUCCESS) {
-					return OSVR_RETURN_FAILURE;
-				}
-				// Display the device information.
-				printf("Opened device with deviceInfo:\n");
-				printDeviceInfo(freespaceId);
+class FreespaceDevice {
+  public:
+    static const unsigned int READ_TIMEOUT_MS = 100;
+    FreespaceDevice(OSVR_PluginRegContext ctx, FreespaceDeviceIdWrapper &&devId,
+                    const char *name)
+        : m_deviceId(std::move(devId)) {
 
-				printf("\nTrying to open device id %d\n", freespaceId);
-				// Prepare to communicate with the device found above
-				rc = freespace_openDevice(freespaceId);
-				if (rc != FREESPACE_SUCCESS) {
-					printf("Error opening device: %d\n", rc);
-					return OSVR_RETURN_FAILURE;
-				}
+        /// Create the initialization options
+        OSVR_DeviceInitOptions opts = osvrDeviceCreateInitOptions(ctx);
 
+        /// Indicate that we'll report tracking.
+        osvrDeviceTrackerConfigure(opts, &m_tracker);
 
-				// Make sure any old messages are cleared out of the system
-				rc = freespace_flush(freespaceId);
-				if (rc != FREESPACE_SUCCESS) {
-					printf("Error flushing device: %d\n", rc);
-					return OSVR_RETURN_FAILURE;
-				}
+        /// Create an asynchronous (threaded) device
+        m_dev.initAsync(ctx, name, opts);
 
-				// Configure the device for motion outputs
-				printf("Sending message to enable motion data.\n");
-				struct freespace_message message;
-				memset(&message, 0, sizeof(message)); // Make sure all the message fields are initialized to 0.
+        /// Send JSON descriptor
+        m_dev.sendJsonDescriptor(com_osvr_Freespace_json);
 
-				/*message.messageType = FREESPACE_MESSAGE_DATAMODECONTROLV2REQUEST;
-				message.dataModeControlV2Request.packetSelect = 8;  // MotionEngine Outout
-				message.dataModeControlV2Request.mode = 0;          // Set full motion
-				message.dataModeControlV2Request.formatSelect = 0;  // MEOut format 0
-				message.dataModeControlV2Request.ff0 = 1;           // Pointer fields
-				message.dataModeControlV2Request.ff3 = 1;           // Angular velocity fields*/
-				message.messageType = FREESPACE_MESSAGE_DATAMODECONTROLV2REQUEST;
-				message.dataModeControlV2Request.packetSelect = 8; // MotionEngine Output
-				message.dataModeControlV2Request.mode = 4; // Full Motion On
-				message.dataModeControlV2Request.formatSelect = 0; // Format 0
-				if (false) {
-					message.dataModeControlV2Request.ff1 = 1; // Enable Linear Acceleration
-					message.dataModeControlV2Request.ff3 = 1; // Enable Angular Velocity
-					message.dataModeControlV2Request.ff0 = 1; // Enable cursor and buttons
-				}
-				if (true) {
-					message.dataModeControlV2Request.ff6 = 1; // Enable Angular Position
-					message.dataModeControlV2Request.ff0 = 1; // Enable cursor and buttons
-				}
+        printf("Registering update callback deviceId=%d\n", m_deviceId.get());
+        /// Sets the update callback
+        m_dev.registerUpdateCallback(this);
+    }
 
-				rc = freespace_sendMessage(freespaceId, &message);
-				if (rc != FREESPACE_SUCCESS) {
-					printf("Could not send message: %d.\n", rc);
-					return OSVR_RETURN_FAILURE;
-				}
-				printf("Registering object for deletion: %d.\n", freespaceId);
-				/// Create our device object, passing the context
-				osvr::pluginkit::registerObjectForDeletion(ctx, new FreespaceDevice(ctx, freespaceId, &deviceInfo, "Freespace"));
-			}
-			return OSVR_RETURN_SUCCESS;
-		}
+    /// In this case, the core spawns a thread, with a loop calling this
+    /// function as long as things are running. So this function waits for the
+    /// next message from the device and passes it on.
+    OSVR_ReturnCode update() {
+        struct freespace_message message;
+        auto ret =
+            freespace_readMessage(m_deviceId.get(), &message, READ_TIMEOUT_MS);
+        if (ret == FREESPACE_ERROR_TIMEOUT ||
+            ret == FREESPACE_ERROR_INTERRUPTED) {
+            // Both timeout and interrupted are ok.
+            // Timeout happens if there aren't any events for a second.
+            // Interrupted happens if you type CTRL-C or if you
+            // type CTRL-Z and background the app on Linux.
+            return OSVR_RETURN_SUCCESS;
+        }
+        if (ret != FREESPACE_SUCCESS) {
+            // This means we didn't have success and we didn't have an excusable
+            // error.
+            // Close communications with the device
+            m_cleanup();
+            return OSVR_RETURN_FAILURE;
+        }
 
-	private:
-		bool _freespace_initialized;
-		int rc; //return code
-		bool m_found;
-		int device_index = 0;
-		/**
-		* printVersionInfo
-		* Common helper function that prints the application running and the version of libfreespace
-		* being used.
-		*
-		* @param appname A pointer to a string containing the application name. Typically from the arguments
-		* passed in to main.
-		*/
-		void printVersionInfo(const char* appname) {
-			printf("%s: Using libfreespace %s\n",
-				appname,
-				freespace_version());
-		}
+        // freespace_printMessage(stdout, &message); // This just prints the
+        // basic message fields
+        if (message.messageType != FREESPACE_MESSAGE_MOTIONENGINEOUTPUT) {
+            printf("freespace OSVR plugin: unhandled message ignored of type: "
+                   "%d\n",
+                   message.messageType);
+            return OSVR_RETURN_SUCCESS;
+        }
 
-		/**
-		* printDeviceInfo
-		* Common helper function that prints the information about a device
-		*
-		* @param id The ID of the device to print the info for.
-		* @param FREESPACE_SUCCESS or an error
-		*/
-		int printDeviceInfo(FreespaceDeviceId id) {
-			struct FreespaceDeviceInfo info;
-			int rc;
+        struct MultiAxisSensor angPos;
+        auto getRet =
+            freespace_util_getAngPos(&message.motionEngineOutput, &angPos);
+        if (getRet != 0) {
+            printf(
+                "freespace OSVR plugin: freespace_util_getAngPos returned %d\n",
+                getRet);
+            return OSVR_RETURN_SUCCESS; // pretend it's harmless.
+        }
 
-			// Retrieve the information for the device
-			rc = freespace_getDeviceInfo(id, &info);
-			if (rc != FREESPACE_SUCCESS) {
-				return rc;
-			}
+        OSVR_OrientationState orientation;
+        osvrQuatSetW(&orientation, angPos.w);
+        osvrQuatSetX(&orientation, angPos.x);
+        osvrQuatSetY(&orientation, angPos.y);
+        osvrQuatSetZ(&orientation, angPos.z);
+        osvrDeviceTrackerSendOrientation(m_dev, m_tracker, &orientation, 0);
 
-			printf("    Device = %s\n    Vendor ID  = 0x%x (%d)\n    Product ID = 0x%x (%d)\n",
-				info.name, info.vendor, info.vendor, info.product, info.product);
+        return OSVR_RETURN_SUCCESS;
+    }
 
-			return FREESPACE_SUCCESS;
-		}
-		void freespaceInit()
-		{
-			if (!_freespace_initialized) {
-				_freespace_initialized = true;
-				rc = freespace_init();
-				if (rc != FREESPACE_SUCCESS) {
-					printf("freespaceInit: failed to init freespace lib. rc=%d\n", rc);
-					_freespace_initialized = false;
-				}
-				else
-				{
-					printf("Freespace library initialized.\n");
-				}
-			}
-		}
-	};
+  private:
+    void m_cleanup() { m_deviceId.close(); }
+    osvr::pluginkit::DeviceToken m_dev;
+    OSVR_TrackerDeviceInterface m_tracker;
+    FreespaceDeviceIdWrapper m_deviceId;
+};
+
+inline std::vector<FreespaceDeviceId> getDeviceList() {
+    std::vector<FreespaceDeviceId> devices;
+    int numFound = 0;
+    std::array<FreespaceDeviceId, FREESPACE_MAXIMUM_DEVICE_COUNT> devicesRaw;
+    auto ret = freespace_getDeviceList(devicesRaw.data(), devicesRaw.size(),
+                                       &numFound);
+    if (ret == FREESPACE_SUCCESS && numFound > 0) {
+        devices.assign(devicesRaw.data(), devicesRaw.data() + numFound);
+    }
+    return devices;
+}
+
+/**
+* printDeviceInfo
+* Common helper function that prints the information about a device
+*
+* @param id The ID of the device to print the info for.
+* @param FREESPACE_SUCCESS or an error
+*/
+int printDeviceInfo(FreespaceDeviceId id) {
+    struct FreespaceDeviceInfo info;
+    int rc;
+
+    // Retrieve the information for the device
+    rc = freespace_getDeviceInfo(id, &info);
+    if (rc != FREESPACE_SUCCESS) {
+        return rc;
+    }
+
+    printf("    Device = %s\n    Vendor ID  = 0x%x (%d)\n    Product ID = "
+           "0x%x (%d)\n",
+           info.name, info.vendor, info.vendor, info.product, info.product);
+
+    return FREESPACE_SUCCESS;
+}
+
+class HardwareDetection {
+  public:
+    HardwareDetection() {}
+
+    FreespaceDeviceIdWrapper attemptDeviceSetup(FreespaceDeviceId id) const {
+        FreespaceDeviceIdWrapper retVal;
+        std::cout << "ID " << id;
+        if (m_handledDevices.find(id) != m_handledDevices.end()) {
+            std::cout << " - Already handled" << std::endl;
+            return retVal;
+        }
+        std::cout << " - Not yet handled." << std::endl;
+        // Display the device information.
+
+        auto ret = printDeviceInfo(id);
+        if (ret != FREESPACE_SUCCESS) {
+            std::cout << " - Couldn't get device info! skipping..."
+                      << std::endl;
+            return retVal;
+        }
+
+        printf("\nTrying to open device id %d\n", id);
+        // Prepare to communicate with the device found above
+
+        FreespaceDeviceIdWrapper dev(id);
+        if (!dev) {
+            return dev;
+        }
+
+        // Make sure any old messages are cleared out of the system
+        ret = freespace_flush(dev.get());
+        if (ret != FREESPACE_SUCCESS) {
+            printf("Error flushing device: code %d\n", ret);
+            return retVal;
+        }
+
+        // Configure the device for motion outputs
+        printf("Sending message to enable motion data.\n");
+        struct freespace_message message = {0};
+
+        message.messageType = FREESPACE_MESSAGE_DATAMODECONTROLV2REQUEST;
+        message.dataModeControlV2Request.packetSelect =
+            8;                                     // MotionEngine Output
+        message.dataModeControlV2Request.mode = 4; // Full Motion On
+        message.dataModeControlV2Request.formatSelect = 0; // Format 0
+
+        message.dataModeControlV2Request.ff6 = 1; // Enable Angular Position
+        message.dataModeControlV2Request.ff0 = 1; // Enable cursor and buttons
+
+        ret = freespace_sendMessage(dev.get(), &message);
+        if (ret != FREESPACE_SUCCESS) {
+            printf("Could not send message: code %d.\n", ret);
+            return retVal;
+        }
+
+        retVal = std::move(dev);
+
+        return retVal;
+    }
+    OSVR_ReturnCode operator()(OSVR_PluginRegContext ctx) {
+        std::cout << "PLUGIN: Got a hardware detection request" << std::endl;
+
+        std::cout << "Scanning for Freespace devices" << std::endl;
+        std::vector<FreespaceDeviceId> devices = getDeviceList();
+        std::cout << "Got " << devices.size() << " devices" << std::endl;
+
+        for (auto id : devices) {
+            auto newDevice = attemptDeviceSetup(id);
+            if (!newDevice) {
+                continue;
+            }
+            std::string name = "Freespace";
+            name += std::to_string(m_handledDevices.size());
+            osvr::pluginkit::registerObjectForDeletion(
+                ctx,
+                new FreespaceDevice(ctx, std::move(newDevice), name.c_str()));
+            m_handledDevices.insert(id);
+        }
+        return OSVR_RETURN_SUCCESS;
+    }
+
+  private:
+    std::set<FreespaceDeviceId> m_handledDevices;
+};
 
 } // namespace
 
 OSVR_PLUGIN(com_osvr_Freespace) {
-	/// Register custom message type
-	//osvrDeviceRegisterMessageType(ctx, "FreespaceDevice", &freespaceMessage);
-	printf("com_osvr_Freespace Plugin loaded. Registering PluginContext\n");
-	osvr::pluginkit::PluginContext context(ctx);
+    std::cout << "com_osvr_Freespace: Plugin loaded, libfreespace version "
+              << freespace_version() << std::endl;
+    osvr::pluginkit::PluginContext context(ctx);
 
-	/// Register a detection callback function object.
-	context.registerHardwareDetectCallback(new HardwareDetection());
+    try {
+        std::unique_ptr<FreespaceLib> libHandle(new FreespaceLib());
+        context.registerObjectForDeletion(libHandle.release());
+    } catch (std::runtime_error &e) {
+        std::cerr << "com_osvr_Freespace: " << e.what() << std::endl;
+        return OSVR_RETURN_FAILURE;
+    }
+    /// Register a detection callback function object.
+    context.registerHardwareDetectCallback(new HardwareDetection());
 
-	return OSVR_RETURN_SUCCESS;
+    return OSVR_RETURN_SUCCESS;
 }
